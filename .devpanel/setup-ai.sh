a15b5eb3148e8178b505840ccbd0a5de314d70d2
Marcus Johansson <me@marcusmailbox.com>
2025-07-09 13:34:04 +0200

    Debug ai settings

--- .devpanel/setup-ai.sh
+++ .devpanel/setup-ai.sh
@@ -24,19 +24,20 @@ if [ -n "${DP_AI_VIRTUAL_KEY:-}" ]; then
   drush -n cset ai.settings default_providers.text_to_speech.provider_id litellm
   drush -n cset ai.settings default_providers.text_to_speech.model_id openai/gpt-4o-mini-realtime-preview
 
+  # Print out the whole configuration for debugging.
+  echo "AI configuration:"
+  drush cget ai.settings --format=yaml
+  echo
   # Check if psql is installed.
   if command -v psql >/dev/null 2>&1; then
     time drush pm:en -y ai_vdb_provider_postgres ai_search
-    echo -n "Setting up the postgres password"
     drush -n key-save postgres_db_password --label="Postgres DB Password" --key-provider=config --key-provider-settings='{
       "key_value": "db"
     }'
-    echo -n "Setting up the postgres database"
     drush -n cset ai_vdb_provider_postgres.settings password postgres_db_password
     if env | grep -q DDEV_PROJECT; then
       drush -n cset ai_vdb_provider_postgres.settings host $PG_HOST
     else
-      echo -n "Setting up the postgres database on localhost"
       drush -n cset ai_vdb_provider_postgres.settings host localhost
     fi
     drush -n cset ai_vdb_provider_postgres.settings port 5432
