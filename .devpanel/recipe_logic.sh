fce1e1b49c50c107534774a789ee17be23d641bb
Marcus Johansson <me@marcusmailbox.com>
2025-07-10 09:20:14 +0200

    Tiktoken

--- .devpanel/recipe_logic.sh
+++ .devpanel/recipe_logic.sh
@@ -12,7 +12,12 @@ drush recipe ../recipes/drupal_module_chatbot_recipe -y
 drush pbai
 
 #== Index 10 nodes.
+echo "Indexing 10 nodes to test..."
 drush sapi-i --limit=10 modules_server
 
+echo "Making sure the tiktoken directory is writable by all users..."
 #== Make sure tiktoken is writable by all.
+if [ ! -d /tmp/tiktoken ]; then
+  sudo mkdir -p /tmp/tiktoken
+fi
 sudo chmod -R 777 /tmp/tiktoken
